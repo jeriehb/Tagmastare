9d071d4173720afb789e088d2497e149241b0ca0
Nardos22 <94529625+Nardos22@users.noreply.github.com>
2022-10-29 20:13:37 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./sass/index.scss";
+
+import  Container  from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
+
+export default function App() {
+    return<>
+  <Container>Hello
+    <Button>Search</Button>
+
+  </Container>
+    </>
+}
